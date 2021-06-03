882860549659be703f4d7e5cba595bfec7f9b9f2
debabratabanerjee <debabratabanerjee021@gmail.com>
2021-06-03 13:14:59 +0530

    added copy to clipboard feature in image upload

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -1,12 +1,26 @@
 import { useState } from 'react';
 import { auth, storage, STATE_CHANGED } from '@lib/firebase';
 import Loader from './Loader';
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { MdContentCopy } from "react-icons/md";
+
+
 
 // Uploads images to Firebase Storage
 export default function ImageUploader() {
   const [uploading, setUploading] = useState(false);
   const [progress, setProgress] = useState(0);
   const [downloadURL, setDownloadURL] = useState(null);
+  const [isCopied, setIsCopied] = useState(false);
+
+
+  //copy function
+  const onCopyText = () => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
 
   // Creates a Firebase Upload Task
   const uploadFile = async (e) => {
@@ -49,8 +63,11 @@ export default function ImageUploader() {
           </label>
         </>
       )}
-
+      <CopyToClipboard text={`![alt](${downloadURL})`} onCopy={onCopyText}>
+            <span>{isCopied ? "Copied!" : <MdContentCopy />}</span>
+          </CopyToClipboard>
       {downloadURL && <code className="upload-snippet">{`![alt](${downloadURL})`}</code>}
+      
     </div>
   );
 }
