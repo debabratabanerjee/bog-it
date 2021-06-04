6c55e14a8432249ef19653c12e1dfd23bf9f233c
debabratabanerjee <debabratabanerjee021@gmail.com>
2021-06-04 14:41:44 +0530

    image testing

--- components/Metatags.js
+++ components/Metatags.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Metatags({
   title = 'The writters blog ',
   description = 'A full fleged blogging site',
-  image = '/featured.png',
+  image = 'https://firebasestorage.googleapis.com/v0/b/blog-it-806bd.appspot.com/o/uploads%2F7YrRtmE216dryEnJA0QO0Pza4og1%2F1622634780888.jpeg?alt=media&token=b5ada2ac-bf9b-4864-a9d3-9a535ee8e3db',
 }) {
   return (
     <Head>
