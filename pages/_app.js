71a0992f0488780e94333c8d27df5d717bf6ff1c
debabratabanerjee <debabratabanerjee021@gmail.com>
2021-06-04 16:40:12 +0530

    trial of pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import '@styles/globals.css';
 import Navbar from '@components/Navbar';
 import { UserContext } from '@lib/context';
@@ -9,6 +10,33 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <UserContext.Provider value={userData}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Written Desk</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/icon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/icon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon"></link>
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Toaster/>
