da05aae5faee0bf5ae028f2b325c3c03b708886e
debabratabanerjee <debabratabanerjee021@gmail.com>
2021-06-06 09:35:57 +0530

    image metatag remove trial

--- components/Metatags.js
+++ components/Metatags.js
@@ -3,7 +3,7 @@
 export default function Metatags({
   title = 'The writters blog ',
   description = 'A full fleged blogging site',
-  image = 'https://firebasestorage.googleapis.com/v0/b/blog-it-806bd.appspot.com/o/logo%20and%20stuff%2Fandroid-chrome-512x512.png?alt=media&token=4bb40530-6c0e-49e5-ab19-8786d1ae3cef',
+  //image = 'https://firebasestorage.googleapis.com/v0/b/blog-it-806bd.appspot.com/o/logo%20and%20stuff%2Fandroid-chrome-512x512.png?alt=media&token=4bb40530-6c0e-49e5-ab19-8786d1ae3cef',
 }) {
   return (
     <Head>
